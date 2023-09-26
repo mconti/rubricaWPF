3066276af849931bdaba211cff37a8efb655d78a
mconti <posta@maurizioconti.com>
2023-09-26 15:04:47 +0200

    .

---
+++ conti.maurizio.4i.rubrica/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using conti.maurizio._4i.rubrica;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace conti.maurizio._4i.rubrica
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Contatto c = new Contatto();
+            c.Numero = 1;
+            c.Nome = "Maurizio";
+            c.Cognome = "Conti";
+
+            Contatto[] Contatti = new Contatto[100];
+            Contatti[0] = c;
+
+            Contatti[1].Nome = "Riccardo";
+            Contatti[1].Cognome = "Bianchi";
+
+        }
+    }
+}
+
+
